a6fbb3269b7305628e8288e5fbc5298cff0aad87
tatecthompson <tatecthompson@gmail.com>
2022-11-29 10:04:25 -0700

    info box added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
+import React, {useState} from "react"
+import Info from "./Info.js";
+import aquarium from "./aquarium.jpg"
 
 function App() {
+
+  const [isOpen, setIsOpen] = useState(false);
+  const toggleInfo = () => {
+    setIsOpen(!isOpen);
+  }
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style = {{backgroundImage: "url(/aquarium.jpg)"}} className="App">
+      <button className="fish" onClick={toggleInfo}>Test Fish</button>
+      {isOpen && <Info
+        handleClose={toggleInfo}
+        content={<div className="info-text">
+          <img className="info-image" src={aquarium} alt ="fish" width="200" height="200"></img>
+          <h2>Fish 1</h2>
+          <h2>Fish 1</h2>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas maximus nibh ante, vel elementum nibh gravida sed. Praesent dictum sed eros vestibulum tempor. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Pellentesque consectetur vulputate urna, at dignissim nisi consectetur in. Phasellus sed ipsum ante. Sed in facilisis tortor. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Pellentesque sodales metus massa, nec pellentesque metus rutrum quis. In vestibulum suscipit ante eu ornare. Vivamus eu tortor ultrices, gravida lacus a, scelerisque leo. Praesent auctor lacinia dui, in bibendum dui egestas nec. Nulla a egestas magna.</p>
+          </div>}
+      />}
     </div>
   );
 }
